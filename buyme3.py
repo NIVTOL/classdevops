7b86c47bb47f50006afa9e86645832d840678c71
NIVTOL <nivtol@gmail.com>
2018-11-26 15:18:13 +0200

    DSD

--- buyme3.py
+++ buyme3.py
@@ -13,6 +13,6 @@ browser.find_element_by_xpath('//*[@id="ember650_chosen"]/div/ul/li[4]').click()
 browser.find_element_by_id('ember674_chosen').click() # choose אזור
 browser.find_element_by_xpath('//*[@id="ember674_chosen"]/div/ul/li[3]').click() # מרכז
 
-browser.find_element_by_id('ember641_chosen').click() # choose אזור
+browser.find_element_by_id('ember641_chosen').click() # choose ssאזור
 browser.find_element_by_xpath('//*[@id="ember641_chosen"]/div/ul/li[2]').click()
 browser.find_element_by_class_name('ui-btn').click()#test
